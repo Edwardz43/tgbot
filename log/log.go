e080ac129c01119fba30d2bfde68f9fe1acc6224
EdLo <b9330315@gmail.com>
2020-03-18 00:20:06 +0800

    [Mod] Change log interface

--- log/log.go
+++ log/log.go
@@ -1,21 +1,12 @@
 package log
 
-import "time"
-
 // Logger defines the log behaviors
 type Logger interface {
-	Debug(Content)
-	Info(Content)
-	Warn(Content)
-	Error(Content)
-	Fatal(Content)
-	Panic(Content)
-	hook() error
-}
-
-// Content is a log content data model
-type Content struct {
-	Message string    `json:"message"`
-	Date    time.Time `json:"date"`
-	Line    int       `json:"line"`
+	INFO(string)
+	DEBUG(string)
+	WARN(string)
+	ERROR(string)
+	FATAL(string)
+	PANIC(string)
+	//hook() error
 }
